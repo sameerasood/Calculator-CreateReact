97d8f2243a99fd5862bedcc3f4936789757c94f8
sameera sood <sameerasood@gmail.com>
2023-04-28 14:54:40 +0100

    percent handle function added

--- calculator-create-react/src/App.js
+++ calculator-create-react/src/App.js
@@ -92,6 +92,18 @@ const App = () => {
     });
   };
 
+  const percentClickHandler = () => {
+    let num = calc.num ? parseFloat(calc.num) : 0;
+    let res = calc.res ? parseFloat(calc.res) : 0;
+
+    setCalc({
+      ...calc,
+      num: (num /= Math.pow(100, 1)),
+      res: (res /= Math.pow(100, 1)),
+      sign: "",
+    });
+  };
+
   return (
     <Wrapper>
       <TimeDate />
